039f7655946d31e99a1d330d77b405cb90c9c019
nikhil <nikhilyagik2004@gmail.com>
2025-02-09 15:44:59 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState ,useRef , useEffect } from 'react'
 import { FaHeart } from "react-icons/fa";
 import './App.css'
 import gsap from "gsap"
-import { Bounce } from 'gsap';
+
 
 function App() {
 
@@ -15,9 +15,10 @@ function App() {
       y: -20,
       stagger: 0.1,
       duration: 0.5,
-      ease: "elastic.out(1, 0.5)",
+      ease: "bounce.inOut(1,0.5)",
     });
   }, [yesButtons]);
+
   const handleNoClick=()=>{
     
     gsap.to(noButtonRef.current,{
@@ -58,10 +59,9 @@ function App() {
            <FaHeart color={"red"} size={40}/></span> </h1>
         }
         {
-          isAccept&& <h1  className='text-5xl text-center font-bold text-black flex items-center'>Love You too!!<span className='ml-0.5'>
+          isAccept&& <h1  className='text-5xl text-center font-bold text-black flex items-center'>yaayyy!! Lets go on a Date<span className='ml-0.5'>
            <FaHeart color={"red"} size={40}/></span></h1>
         }
-
       </div>
       <div className='flex justify-center my-5'>
         
@@ -94,10 +94,7 @@ function App() {
           >
             no</button>
            }
-
           </div>
-     
-    
    </>
   )
 }
